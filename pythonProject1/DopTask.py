8d067ce4efc727c77e756cece4a47466652a22ff
Vasteron-Git <Larmax@yandex.ru>
2024-09-01 19:42:31 +0300

    GitIgnore4

--- pythonProject1/DopTask.py
+++ pythonProject1/DopTask.py
@@ -1,7 +1,7 @@
 
-# Список
+# List
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-# Множество
+# Set
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 list_students = list(students)
@@ -14,4 +14,4 @@ for i in range(0, len(list_students) - 1):
 # students_grades[list_students[2]] = {sum(grades[2]) / len(grades[2])}
 # students_grades[list_students[3]] = {sum(grades[3]) / len(grades[3])}
 # students_grades[list_students[4]] = {sum(grades[4]) / len(grades[4])}
-print("Averadg score of students:", students_grades)
+print("Average score of students:", students_grades)
