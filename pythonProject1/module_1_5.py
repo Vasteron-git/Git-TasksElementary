dd69e545366af054028f314b3f8fa6b6bed8514e
Vasteron-Git <Larmax@yandex.ru>
2024-09-01 13:40:35 +0300

    GitIgnore5

--- pythonProject1/module_1_5.py
+++ pythonProject1/module_1_5.py
@@ -1,13 +1,13 @@
 
 # 2nd task
-immutable_var = 10,15,20, False, "Gznm", "Python"
+immutable_var = 10,15,20, False, "Пять", "Python"
 print("immutable_var:",immutable_var)
 # 3rd task
 print(type(immutable_var))
 # immutable_var[1]=16
 # При попытке изменения быдет выдавать ошибку, так как переменную типа tuple изменять нельзя
 # 4th task
-mutable_list = [10,15,20, False, "Gznm", "Python"]
+mutable_list = [10,15,20, False, "Пять", "Python"]
 mutable_list[1] = 16
 print("mutable_list:", mutable_list)
 
